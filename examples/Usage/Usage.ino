1ecc5271f9fd43bc65356a29680003781f06a6d1
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2025-05-10 11:22:56 +0800

    Update Usage.ino

--- examples/Usage/Usage.ino
+++ examples/Usage/Usage.ino
@@ -1,29 +1,46 @@
-#include "Bonezegei_JSON.h"
+/*
+  Simple JSON encoder and decoder
+  Author: Bonezegei (Jofel Batutay)
+  Date : May 2025
 
+  Note!: it only works on the serialize format like in this example
+*/
+
+#include <Bonezegei_JSON.h>
 Bonezegei_JSON json;
-char response[128];
+
+char decode[128];
+char encode[128];
 
 void setup() {
   Serial.begin(9600);
 
-  sprintf(json.data, "{\"cmd\":\"serial commands  \",\"temp\":24.5}");
-
-  Serial.println(json.data);
-  Serial.println(json.get("cmd"));
-
-  Serial.println(json.get("temp"));
-
-  json.addData(response, "cmd", "application sdsad Hello ");
-  json.addData(response, "coin", "1");
-  json.addData(response, "temperature", 24.143);
-  json.addData(response, "count", 24);
-  Serial.println(response);
-
-  Serial.println(json.get(response,"cmd"));
+  // set decode array
+  sprintf(decode, "{\"cmd\":\"serial commands  \",\"temp\":24.5}");
+  Serial.println(decode);
+  // Decode 
+  // param 1 location of the json string
+  // param 2 keyword
+  // get will return the value of the keyword
+  Serial.println(json.get(decode, "cmd"));
+  Serial.println(json.get(decode, "temp"));
+  Serial.println();
 
 
+  // Encode
+  // param 1 array 
+  // param 2 keyword
+  // param 3 value
+  json.addData(encode, "cmd", "Hello World");
+  json.addData(encode, "coin", "1");
+  json.addData(encode, "temperature", 24.143);
+  json.addData(encode, "count", 24);
+  Serial.print("encoded json: ");
+  Serial.println(encode);
+
+  // 
+  Serial.println(json.get(encode,"cmd"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
